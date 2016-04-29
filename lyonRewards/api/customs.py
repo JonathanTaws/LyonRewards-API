d45f4048ccc05962f6fb76b34e3d5c5782aa13f3
Romathonat <romain.mathonat@gmail.com>
2016-04-29 15:29:16 +0200

    forgot the custom file

---
+++ lyonRewards/api/customs.py
@@ -0,0 +1,21 @@
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from api.models import Profile
+from api.serializers import ProfileSerializer
+
+
+class CustomObtainAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+
+        profil = Profile.objects.get(user=user)
+
+        data_return = ProfileSerializer(profil).data
+        return Response({'token': token.key, 'user': data_return})
+
+
+obtain_auth_token = CustomObtainAuthToken.as_view()
