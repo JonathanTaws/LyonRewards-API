079db19af2b6b133431d0cad1d87d3c6c00751fa
Romathonat <romain.mathonat@gmail.com>
2016-04-30 16:35:41 +0200

    restricted acces user

--- lyonRewards/api/views.py
+++ lyonRewards/api/views.py
@@ -8,6 +8,7 @@ from django.shortcuts import render, get_object_or_404
 from rest_framework import viewsets, status, mixins
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.decorators import detail_route, api_view
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.authtoken.models import Token
@@ -111,6 +112,8 @@ class EventViewSet(mixins.CreateModelMixin,
 class ProfileViewSet(viewsets.ModelViewSet):
     serializer_class = ProfileSerializer
     queryset = Profile.objects.all()
+
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
     @detail_route(methods=['post'])
     def registerGCM(self, request):
